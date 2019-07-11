a8b9d1867cb048a677dcb5ac4e797b2ade340ee5
Vlad Shyrinov <vlad.shyrinov@gmail.com>
2019-07-11 23:42:15 +0200

    changed recursive_sum and added recursive_count

--- recursive_sum.py
+++ recursive_sum.py
@@ -1,9 +1,7 @@
 def recursive_sum(arr):
-    arr_len = len(arr)
-    if arr_len == 1:
-        return arr[0]
-    else:
-        return arr.pop(arr_len-1) + recursive_sum(arr)
+    if arr == []:
+        return 0
+    return arr[0] + recursive_sum(arr[1:])
 
 def main():
     print(recursive_sum([3,2,-1,4,56,1,8]))
